bc8d44d5027d5016b21831a9a7fb2fed0dbfca44
magu <m2aguc@gmail.com>
2013-10-12 20:05:31 +0900

    itの余分な改行を削除

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -3,11 +3,6 @@ require_relative '../lib/message_filter'
 
 describe MessageFilter, "with argument foo" do
   subject { MessageFilter.new('foo') }
-  it {
-    should be_detect('hello from foo')
-  }
-
-  it {
-    should_not be_detect('hello, world')
-  }
+  it { should be_detect('hello from foo') } 
+  it { should_not be_detect('hello, world') }
 end
