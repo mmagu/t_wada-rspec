45c82d2861b85d10789a7755730611ccbeb9ad25
magu <m2aguc@gmail.com>
2013-10-15 06:57:23 +0900

    複数のNGワードを設定できるように、改修作業に着手。 まずはテストから記述

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,8 +1,13 @@
 require 'rspec'
 require_relative '../lib/message_filter'
 
-describe MessageFilter, "with argument foo" do
+describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
   it { should be_detect('hello from foo') } 
   it { should_not be_detect('hello, world') }
+end
+
+describe MessageFilter, 'with argument "foo","bar"' do
+  subject { MessageFilter.new('foo','bar') }
+  it { should be_detect('hello from bar') } 
 end
