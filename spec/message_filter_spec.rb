172d4ca6b6f1c6eab314dd70acc6b8a1b9ccc841
magu <m2aguc@gmail.com>
2013-10-15 07:05:17 +0900

    引数を２個受け取るように実装を変更。 テストコードが通るように、まずはベタにプロダクションコードを実装。

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -4,10 +4,10 @@ require_relative '../lib/message_filter'
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
   it { should be_detect('hello from foo') } 
-  it { should_not be_detect('hello, world') }
+  it { should_not be_detect('hello, world!') }
 end
 
 describe MessageFilter, 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo','bar') }
-  it { should be_detect('hello from bar') } 
+  it { should be_detect('hello from foo') } 
 end
